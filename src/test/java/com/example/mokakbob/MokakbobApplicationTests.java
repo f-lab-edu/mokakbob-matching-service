0741ad8081dbb9c0d2843a7077c102403335e6c0
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-07 10:52:23 +0900

    init: 프로젝트 초기 Spring Boot 구조 세팅

---
+++ src/test/java/com/example/mokakbob/MokakbobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mokakbob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokakbobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
