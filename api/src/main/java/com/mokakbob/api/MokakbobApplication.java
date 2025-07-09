6459a390c65c4e5f6c4af6c23a5c1b93e1f08afa
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-09 11:23:50 +0900

    [Init] 멀티 모듈 프로젝트 생성 및 그에 따른 build.gradle, setting.gradle 세팅
    * 부가로 jpa dependency 확인을 위해 h2 db 의존성 추가하였습니다.

--- api/src/main/java/com/mokakbob/api/MokakbobApplication.java
+++ api/src/main/java/com/mokakbob/api/MokakbobApplication.java
@@ -1,13 +1,11 @@
-package com.example.mokakbob;
+package com.mokakbob.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MokakbobApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MokakbobApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MokakbobApplication.class, args);
+    }
 }
