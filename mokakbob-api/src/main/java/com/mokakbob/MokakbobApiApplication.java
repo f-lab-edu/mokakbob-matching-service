af7e000ca16bbf8b547e22d19877e8da42c61280
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-16 21:34:05 +0900

    [Feat] BaseEntity 구현
    * Spring Data Jpa 방식을 이용해 BaseEntity 구현.
    * `@EnableJpaAuditing` 어노테이션 사용을 위해 `api` / `batch` 모듈에 jpa 의존성 추가

--- mokakbob-api/src/main/java/com/mokakbob/MokakbobApiApplication.java
+++ mokakbob-api/src/main/java/com/mokakbob/MokakbobApiApplication.java
@@ -2,8 +2,10 @@ package com.mokakbob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MokakbobApiApplication {
 
     public static void main(String[] args) {
