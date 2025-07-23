b2c3ce78ca7ced52be0833e5b424c5b525d835f5
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-23 15:43:22 +0900

    [Feat] 책임 분리에 따른 파일 구조 수정 및 의존성 추가

---
+++ mokakbob-api/src/main/java/com/mokakbob/auth/infrastructure/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package com.mokakbob.auth.infrastructure;
+
+import com.mokakbob.common.exception.DomainException;
+import com.mokakbob.domain.member.domain.auth.TokenProvider;
+import com.mokakbob.domain.member.exception.MemberErrorCode;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider implements TokenProvider {
+
+    private final Key secretKey;
+    private final long expirationPeriod;
+
+    public JwtTokenProvider(
+            @Value("${jwt.secret}") String secretKey,
+            @Value("${jwt.expiration-period}") long expirationPeriod
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.expirationPeriod = expirationPeriod;
+    }
+
+    @Override
+    public String create(Long memberId) {
+        Date now = new Date();
+        Date expire = new Date(now.getTime() + expirationPeriod);
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(memberId))
+                .setIssuedAt(now)
+                .setExpiration(expire)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    @Override
+    public Long extractMemberId(String token) {
+        Claims claims = parseToken(token);
+        return Long.valueOf(claims.getSubject());
+    }
+
+    private Claims parseToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+        } catch (ExpiredJwtException e) {
+            throw new DomainException(MemberErrorCode.TOKEN_EXPIRED);
+        } catch (SignatureException e) {
+            throw new DomainException(MemberErrorCode.TOKEN_INVALID_SIGNATURE);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new DomainException(MemberErrorCode.TOKEN_INVALID);
+        }
+    }
+}
