6ef479d4fdb8e57374f14231cc64bc566d191a06
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-23 21:50:30 +0900

    [Feat] 비동기 설정 구현 및 메일 전송 로직 비동기 처리

--- mokakbob-api/src/main/java/com/mokakbob/auth/infrastructure/SmtpEmailSender.java
+++ mokakbob-api/src/main/java/com/mokakbob/auth/infrastructure/SmtpEmailSender.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +17,7 @@ public class SmtpEmailSender implements EmailSender {
     private final JavaMailSender javaMailSender;
 
     @Override
+    @Async
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = makeInfo(to, subject, text);
 
