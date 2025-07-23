849ad2ae955135dcf11eec01b632fef90a1971fc
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-23 23:00:50 +0900

    [Feat] retry 로직 구현 및 recover 로그 로직 구현

--- mokakbob-api/src/main/java/com/mokakbob/config/AsyncConfig.java
+++ mokakbob-api/src/main/java/com/mokakbob/config/AsyncConfig.java
@@ -1,7 +1,10 @@
 package com.mokakbob.config;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,15 +12,17 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
+@Slf4j
 public class AsyncConfig implements AsyncConfigurer {
 
-    @Override
-    public Executor getAsyncExecutor() {
+    @Bean(name = "EmailExecutor")
+    public Executor emailExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
         executor.setMaxPoolSize(10);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("EmailAsync-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
@@ -25,7 +30,8 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return (ex, method, params) -> {
-            System.err.println("비동기 예외 발생 " + method.getName());
+            log.info("비동기 예외 발생 {}", method.getName());
+            log.info("Exception message: " + ex.getMessage());
             ex.printStackTrace();
         };
     }
