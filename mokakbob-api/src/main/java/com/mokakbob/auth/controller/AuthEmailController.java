9c4112c2424113be7dce8582c26b77f344de53e6
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-24 15:36:40 +0900

    [Feat] 이메일 코드 확인을 위한 컨트롤러 로직 작성

--- mokakbob-api/src/main/java/com/mokakbob/auth/controller/AuthEmailController.java
+++ mokakbob-api/src/main/java/com/mokakbob/auth/controller/AuthEmailController.java
@@ -1,5 +1,6 @@
 package com.mokakbob.auth.controller;
 
+import com.mokakbob.auth.controller.request.EmailCodeRequest;
 import com.mokakbob.domain.member.service.auth.EmailAuthService;
 import com.mokakbob.auth.controller.request.EmailSendRequest;
 import jakarta.validation.Valid;
@@ -17,9 +18,16 @@ public class AuthEmailController {
 
     private final EmailAuthService emailAuthService;
 
-    @PostMapping("/send")
+    @PostMapping("/email/send")
     public ResponseEntity<Void> sendEmailVerificationCode(@Valid @RequestBody EmailSendRequest request) {
         emailAuthService.sendEmail(request.email());
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PostMapping("/email/verify")
+    public ResponseEntity<Void> verifyEmailCode(@Valid @RequestBody EmailCodeRequest request) {
+        emailAuthService.checkEmailCode(request.email(), request.code());
         return ResponseEntity.ok()
                 .build();
     }
