858f762186e645b38f30547873e5c5865cf52e06
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-25 23:09:48 +0900

    [Feat] api 버저닝 적용

---
+++ mokakbob-api/src/main/java/com/mokakbob/common/path/EmailApiPath.java
@@ -0,0 +1,9 @@
+package com.mokakbob.common.path;
+
+public class EmailApiPath {
+
+    private static final String BASE = ApiVersion.V1 + "/email";
+
+    public static final String SEND = BASE + "/send";
+    public static final String VERIFY = BASE + "/verify";
+}
