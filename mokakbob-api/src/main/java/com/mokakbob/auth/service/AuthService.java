0c8caa6353a978dae3cd84a0687d44b77848f19c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-30 16:00:31 +0900

    [Feat] BCryptPasswordEncoder를 사용한 비밀번호 암호화 로직 구현 및 회원 정보 저장 로직 구현

--- mokakbob-api/src/main/java/com/mokakbob/auth/service/AuthService.java
+++ mokakbob-api/src/main/java/com/mokakbob/auth/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.mokakbob.auth.service;
 
+import com.mokakbob.domain.member.domain.Member;
+import com.mokakbob.domain.member.domain.vo.MemberPreference;
 import com.mokakbob.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
+    public Member signUp(String email, String password, String nickName, MemberPreference preference) {
+        String passwordEnc = passwordEncoder.encode(password);
 
+        return memberService.createMember(
+                email,
+                passwordEnc,
+                nickName,
+                preference
+        );
+    }
 }
--- mokakbob-core/src/main/java/com/mokakbob/domain/member/service/MemberService.java
+++ mokakbob-core/src/main/java/com/mokakbob/domain/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.mokakbob.domain.member.service;
 
 import com.mokakbob.common.exception.DomainException;
+import com.mokakbob.domain.member.domain.Member;
+import com.mokakbob.domain.member.domain.vo.MemberPreference;
 import com.mokakbob.domain.member.exception.MemberErrorCode;
 import com.mokakbob.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,26 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private static final int DEFAULT_SCORE = 50;
+    private static final int DEFAULT_POINT = 2000;
+
     private final MemberRepository memberRepository;
+
+    public Member createMember(String email, String passwordEnc, String nickName, MemberPreference preference) {
+        validateDuplicateEmail(email);
+        validateDuplicateNickName(nickName);
+
+        Member member = Member.builder()
+                .email(email)
+                .passwordEnc(passwordEnc)
+                .nickname(nickName)
+                .preference(preference)
+                .score(DEFAULT_SCORE)
+                .depositPoint(DEFAULT_POINT)
+                .build();
+
+        return memberRepository.save(member);
+    }
 
     private void validateDuplicateEmail(String email) {
         if (memberRepository.existsByEmail(email)) {
