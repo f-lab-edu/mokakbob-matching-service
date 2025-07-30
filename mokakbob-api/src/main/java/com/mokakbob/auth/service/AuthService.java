4929d37b521d4f137c6d5f8a210eca12d90cd1c0
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-30 15:42:42 +0900

    [Feat] 이메일 및 닉네임 중복 검증 로직 작성

--- mokakbob-api/src/main/java/com/mokakbob/auth/service/AuthService.java
+++ mokakbob-api/src/main/java/com/mokakbob/auth/service/AuthService.java
@@ -1,7 +1,16 @@
 package com.mokakbob.auth.service;
 
+import com.mokakbob.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final MemberService memberService;
+
+
 }
--- mokakbob-core/src/main/java/com/mokakbob/domain/member/service/MemberService.java
+++ mokakbob-core/src/main/java/com/mokakbob/domain/member/service/MemberService.java
@@ -1,7 +1,28 @@
 package com.mokakbob.domain.member.service;
 
+import com.mokakbob.common.exception.DomainException;
+import com.mokakbob.domain.member.exception.MemberErrorCode;
+import com.mokakbob.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    private void validateDuplicateEmail(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new DomainException(MemberErrorCode.DUPLICATE_EMAIL);
+        }
+    }
+
+    private void validateDuplicateNickName(String nickName) {
+        if (memberRepository.existsByNickname(nickName)) {
+            throw new DomainException(MemberErrorCode.DUPLICATE_NICKNAME);
+        }
+    }
 }
