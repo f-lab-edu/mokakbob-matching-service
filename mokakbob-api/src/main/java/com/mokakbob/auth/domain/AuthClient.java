1e81e99bb5709c3b315aef205a8550e0a8b76558
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-02 00:38:51 +0900

    [Feat] 깃허브 authClient 구현

---
+++ mokakbob-api/src/main/java/com/mokakbob/auth/domain/AuthClient.java
@@ -0,0 +1,6 @@
+package com.mokakbob.auth.domain;
+
+public interface AuthClient {
+
+    String getLoginUrl();
+}
---
+++ mokakbob-api/src/main/java/com/mokakbob/auth/infrastructure/GitHubAuthClient.java
@@ -0,0 +1,34 @@
+package com.mokakbob.auth.infrastructure;
+
+import com.mokakbob.auth.domain.AuthClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class GitHubAuthClient implements AuthClient {
+
+    private static final String GITHUB_LOGIN_URL = "https://github.com/login/oauth/authorize";
+
+    @Value("${oauth.github.client.idl}")
+    private String clientId;
+
+    @Value("${oauth.github.client.secretl}")
+    private String clientSecret;
+
+    @Value("${oauth.github.redirect.url}")
+    private String redirectUrl;
+
+    @Value("${oauth.github.scope}")
+    private String scope;
+
+    @Override
+    public String getLoginUrl() {
+        return UriComponentsBuilder.fromUriString(GITHUB_LOGIN_URL)
+                .queryParam("client_id", clientId)
+                .queryParam("redirect_uri", clientSecret)
+                .queryParam("scope", scope)
+                .build()
+                .toUriString();
+    }
+}
