f0ae0b4d1b4a4f18f39ce2c7374d95ec3241489a
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-02 22:14:11 +0900

    [Feat] api path 삭제 및 그에 따른 변경사항 수정

--- mokakbob-api/src/main/java/com/mokakbob/common/path/auth/GitHubOauthApiPath.java
+++
@@ -1,9 +0,0 @@
-package com.mokakbob.common.path.auth;
-
-import com.mokakbob.common.path.ApiVersion;
-
-public class GitHubOauthApiPath {
-
-    public static final String BASE = ApiVersion.V1 + "/oauth/github";
-    public static final String URL = BASE + "/url";
-}
