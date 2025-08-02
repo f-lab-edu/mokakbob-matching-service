e465b4a454c53209008b4d4f79d41874625a9972
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-02 23:03:44 +0900

    [Feat] 소셜 로그인 콜백 컨트롤러 로직 작성

--- mokakbob-api/src/main/java/com/mokakbob/auth/controller/GitHubOauthController.java
+++ mokakbob-api/src/main/java/com/mokakbob/auth/controller/GitHubOauthController.java
@@ -1,11 +1,20 @@
 package com.mokakbob.auth.controller;
 
+import com.mokakbob.auth.controller.request.OauthCodeRequest;
+import com.mokakbob.auth.controller.response.LoginResponse;
+import com.mokakbob.auth.controller.response.SignUpRequireResponse;
 import com.mokakbob.auth.controller.response.UrlResponse;
 import com.mokakbob.auth.service.GitHubAuthService;
-import com.mokakbob.common.path.auth.GitHubOauthApiPath;
+import com.mokakbob.auth.service.TokenService;
+import com.mokakbob.auth.service.response.MemberExistResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,11 +22,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class GitHubOauthController {
 
     private final GitHubAuthService gitHubAuthService;
+    private final TokenService tokenService;
 
-    @GetMapping(GitHubOauthApiPath.URL)
+    @GetMapping("/oauth/github/url")
     public ResponseEntity<UrlResponse> getGithubLoginUrl() {
         String url = gitHubAuthService.getLoginUrl();
 
         return ResponseEntity.ok(new UrlResponse(url));
+    }
+
+    @PostMapping("/oauth/github/callback")
+    public ResponseEntity<?> callback(
+            @RequestBody OauthCodeRequest request,
+            HttpServletResponse response
+    ) {
+        MemberExistResponse MemberExistResponse = gitHubAuthService.loginOrSignUp(request.code());
+
+        if (MemberExistResponse.isMember()) {
+            Long memberId = MemberExistResponse.memberId();
+            String accessToken = tokenService.createAccessToken(memberId);
+            tokenService.createRefreshToken(memberId, response);
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.set("Authorization", "Bearer " + accessToken);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .headers(httpHeaders)
+                    .body(new LoginResponse(
+                            memberId,
+                            MemberExistResponse.email(),
+                            MemberExistResponse.nickName(),
+                            MemberExistResponse.profileImage()
+                    ));
+        } else {
+            return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
+                    .body(new SignUpRequireResponse(
+                            MemberExistResponse.email(),
+                            MemberExistResponse.nickName(),
+                            MemberExistResponse.profileImage()
+                    ));
+        }
     }
 }
