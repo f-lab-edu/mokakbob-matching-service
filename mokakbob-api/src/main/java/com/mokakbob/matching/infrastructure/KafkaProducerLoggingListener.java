3cc7cf76734d6105c70a27b9929be3876927778c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-06 22:13:30 +0900

    [Feat] kafka producer 생성을 위한 설정 구현

---
+++ mokakbob-api/src/main/java/com/mokakbob/matching/infrastructure/KafkaProducerLoggingListener.java
@@ -0,0 +1,23 @@
+package com.mokakbob.matching.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.support.ProducerListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaProducerLoggingListener implements ProducerListener<String, Object> {
+
+    @Override
+    public void onSuccess(ProducerRecord<String, Object> record, RecordMetadata metadata) {
+        log.info("Kafka 메시지 전송 성공 - Topic: {}, Partition: {}, Offset: {}",
+                metadata.topic(), metadata.partition(), metadata.offset());
+    }
+
+    @Override
+    public void onError(ProducerRecord<String, Object> record, RecordMetadata metadata, Exception exception) {
+        log.error("Kafka 메시지 전송 실패", exception);
+    }
+}
