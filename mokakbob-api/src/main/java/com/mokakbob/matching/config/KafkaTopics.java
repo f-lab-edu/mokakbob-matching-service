89e91d924bdcc569413641730f4a78e4eb3b5e98
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-06 22:25:24 +0900

    [Feat] kafka 토픽 발급 로직 구현

---
+++ mokakbob-api/src/main/java/com/mokakbob/matching/config/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.mokakbob.matching.config;
+
+public class KafkaTopics {
+
+    public static final String MATCHING_PARTICIPATE = "matching.participate";
+    public static final String MATCHING_FOUND = "matching.found";
+    public static final String MATCHING_SUCCESS = "matching.success";
+}
--- mokakbob-api/src/main/java/com/mokakbob/matching/service/MatchingStartService.java
+++ mokakbob-api/src/main/java/com/mokakbob/matching/service/MatchingStartService.java
@@ -5,13 +5,17 @@ import com.mokakbob.domain.matching.domain.vo.MatchingCategory;
 import com.mokakbob.domain.matching.service.MatchingService;
 import com.mokakbob.domain.member.domain.Member;
 import com.mokakbob.domain.member.service.MemberService;
+import com.mokakbob.matching.config.KafkaTopics;
 import com.mokakbob.matching.domain.CategoryQueueStore;
 import com.mokakbob.matching.domain.ParticipantGeoStore;
 import com.mokakbob.matching.exception.MatchingErrorCode;
 import com.mokakbob.matching.infrastructure.ParticipantRedisStore;
+import com.mokakbob.matching.service.event.MatchingParticipateEvent;
 import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,22 +28,23 @@ public class MatchingStartService {
     private final MemberService memberService;
     private final MatchingService matchingService;
     private final CategoryQueueStore categoryQueueStore;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void startMatching(double lat, double lng, MatchingCategory category, int participantCount, Long memberId) {
+    @Transactional
+    public void participateMatching(double lat, double lng, MatchingCategory category, int participantCount,
+                                    Long memberId) {
         validateExistParticipating(memberId);
         deducePoint(memberId);
 
-        matchingService.saveMatchingRequest(
-                memberId,
-                category,
-                participantCount,
-                new BigDecimal(lat),
-                new BigDecimal(lng)
-        );
+        matchingService.saveMatchingRequest(memberId, category, participantCount, new BigDecimal(lat),
+                new BigDecimal(lng));
 
         geoStore.addUserLocation(memberId, lng, lat);
         participantStore.transitionToParticipating(memberId);
         categoryQueueStore.addToQueue(category, participantCount, memberId);
+
+        MatchingParticipateEvent event = new MatchingParticipateEvent(memberId, lat, lng, category, participantCount);
+        kafkaTemplate.send(KafkaTopics.MATCHING_PARTICIPATE, event);
     }
 
     private void deducePoint(Long memberId) {
@@ -48,7 +53,7 @@ public class MatchingStartService {
     }
 
     private void validateExistParticipating(Long memberId) {
-        if(participantStore.isAlreadyParticipating(memberId)) {
+        if (participantStore.isAlreadyParticipating(memberId)) {
             throw new ApiException(MatchingErrorCode.EXIST_MATCHING);
         }
     }
