0bed6cab49827418b44dfe87abaf3f3b00ff59d7
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-07 23:12:36 +0900

    [Feat] 매칭 시작 api path 변경

--- mokakbob-api/src/main/java/com/mokakbob/common/path/matching/MatchingPath.java
+++ mokakbob-api/src/main/java/com/mokakbob/common/path/matching/MatchingPath.java
@@ -2,5 +2,5 @@ package com.mokakbob.common.path.matching;
 
 public class MatchingPath {
 
-    public static final String PARTICIPATE = "/api/v1/matchings/participation";
+    public static final String PARTICIPATE = "/api/v1/matchings/participate";
 }
