3775178ae1dcd49ea1bc510a115d6d8981b5f1e1
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-09 23:25:04 +0900

    [Feat] Outbox 필드 정의 및 생성

---
+++ mokakbob-core/src/main/java/com/mokakbob/domain/matching/domain/MatchingRequestOutbox.java
@@ -0,0 +1,38 @@
+package com.mokakbob.domain.matching.domain;
+
+
+import com.mokakbob.common.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class MatchingRequestOutbox extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String aggregateId;
+
+    @Column(nullable = false)
+    private String eventType;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String payload;
+
+    @Column(nullable = false)
+    private String topic;
+}
