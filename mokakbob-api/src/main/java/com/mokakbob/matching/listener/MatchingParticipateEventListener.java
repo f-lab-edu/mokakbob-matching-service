173b13ee21772306368b9ac2c129fb2d65e89d39
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-11 21:40:49 +0900

    [Feat] kafka 토픽 발급 시, 오류 처리를 위한 로그 남기는 로직 구현

--- mokakbob-api/src/main/java/com/mokakbob/matching/listener/MatchingParticipateEventListener.java
+++ mokakbob-api/src/main/java/com/mokakbob/matching/listener/MatchingParticipateEventListener.java
@@ -3,24 +3,43 @@ package com.mokakbob.matching.listener;
 import com.mokakbob.cache.CategoryQueueStore;
 import com.mokakbob.cache.ParticipantGeoStore;
 import com.mokakbob.cache.ParticipantStore;
+import com.mokakbob.matching.constant.KafkaTopics;
 import com.mokakbob.matching.service.event.MatchingParticipateEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MatchingParticipateEventListener {
 
     private final ParticipantStore participantStore;
     private final ParticipantGeoStore geoStore;
     private final CategoryQueueStore categoryQueueStore;
+    private final KafkaTemplate<String, MatchingParticipateEvent> kafkaTemplate;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleMatchingParticipateEvent(MatchingParticipateEvent event) {
+        sendKafkaTopic(event);
+
         participantStore.transitionToParticipating(event.memberId());
         geoStore.addMemberLocation(event.memberId(), event.lng(), event.lat());
         categoryQueueStore.addToQueue(event.category(), event.participantCount(), event.memberId());
+    }
+
+    private void sendKafkaTopic(MatchingParticipateEvent event) {
+        try {
+            kafkaTemplate.send(KafkaTopics.MATCHING_PARTICIPATE, event);
+        } catch (Exception e) {
+            log.error("카프카 토픽 발급 실패: {}", e.getMessage());
+            log.error("실패한 이벤트: {}", event);
+
+            log.error("재처리 필요 정보 - memberId: {}, lat: {}, lng: {}, category: {}, participantCount: {}",
+                    event.memberId(), event.lat(), event.lng(), event.category(), event.participantCount());
+        }
     }
 }
--- mokakbob-core/src/main/java/com/mokakbob/domain/outbox/repository/OutboxRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mokakbob.domain.outbox.repository;
-
-import com.mokakbob.domain.outbox.domain.Outbox;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OutboxRepository extends JpaRepository<Outbox, Long> {
-}
