f20ae4a0d9566e5dcfb70481cc921f151c609f35
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-23 01:12:28 +0900

    [Feat] MatchingParticipateConsumer 뼈대 코드 작성

---
+++ mokakbob-consumer/src/main/java/com/mokakbob/matching/consumer/MatchingParticipateConsumer.java
@@ -0,0 +1,36 @@
+package com.mokakbob.matching.consumer;
+
+import com.mokakbob.domain.matching.event.MatchingParticipateEvent;
+import com.mokakbob.matching.service.MatchingParticipateService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MatchingParticipateConsumer {
+
+    private final MatchingParticipateService participateService;
+
+    @KafkaListener(
+            topics = "${app.topic.matching-participate:matching.participate}",
+            containerFactory = "matchingParticipateKafkaListenerContainerFactory"
+    )
+    public void onMessage(ConsumerRecord<String, MatchingParticipateEvent> record, Acknowledgment ack) {
+        final String key = record.key();
+        final MatchingParticipateEvent event = record.value();
+
+        try {
+            participateService.participateMatching(event);
+            ack.acknowledge();
+            log.info("[OK] key={}, partition={}, offset={}", key, record.partition(), record.offset());
+        } catch (Exception e) {
+            log.error("[ERR] key={}, partition={}, offset={}, cause={}", key, record.partition(), record.offset(), e.toString());
+            throw e;
+        }
+    }
+}
---
+++ mokakbob-consumer/src/main/java/com/mokakbob/matching/service/MatchingParticipateService.java
@@ -0,0 +1,12 @@
+package com.mokakbob.matching.service;
+
+import com.mokakbob.domain.matching.event.MatchingParticipateEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchingParticipateService {
+
+    public void participateMatching(MatchingParticipateEvent event) {
+        // to do
+    }
+}
