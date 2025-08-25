da2105616c78fcf7452665608ffdd2bc047c45ba
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-25 22:11:14 +0900

    [Feat] 매칭 대기열 인원 확인 로직 구현

--- mokakbob-consumer/src/main/java/com/mokakbob/matching/service/MatchingParticipateService.java
+++ mokakbob-consumer/src/main/java/com/mokakbob/matching/service/MatchingParticipateService.java
@@ -1,12 +1,25 @@
 package com.mokakbob.matching.service;
 
+import com.mokakbob.cache.CategoryQueueStore;
+import com.mokakbob.cache.ParticipantGeoStore;
+import com.mokakbob.cache.ParticipantStore;
 import com.mokakbob.domain.matching.event.MatchingParticipateEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MatchingParticipateService {
 
+    private final ParticipantStore participantStore;
+    private final ParticipantGeoStore geoStore;
+    private final CategoryQueueStore queueStore;
+
     public void participateMatching(MatchingParticipateEvent event) {
-        // to do
+        Long memberId = event.memberId();
+
+        if (!queueStore.hasEnoughForMatching(event.category(), event.participantCount())) {
+            return;
+        }
     }
 }
