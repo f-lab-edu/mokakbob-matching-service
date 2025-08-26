827392ef081ecd34ce5d0f24c5eba4b23b90e98c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-08-26 23:29:58 +0900

    [Feat] MatchingFoundConsumer 뼈대 구현

---
+++ mokakbob-consumer/src/main/java/com/mokakbob/matching/consumer/MatchingFoundConsumer.java
@@ -0,0 +1,24 @@
+package com.mokakbob.matching.consumer;
+
+import com.mokakbob.domain.matching.event.MatchingFoundEvent;
+import com.mokakbob.topic.KafkaTopic;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MatchingFoundConsumer {
+
+    @KafkaListener(
+            topics = KafkaTopic.MATCHING_FOUND,
+            containerFactory = "matchingFoundKafkaListenerContainerFactory"
+    )
+    public void consume(MatchingFoundEvent event) {
+        log.info("[Kafka] MATCHING_FOUND 이벤트 수신: {}", event);
+
+        // to do
+    }
+}
