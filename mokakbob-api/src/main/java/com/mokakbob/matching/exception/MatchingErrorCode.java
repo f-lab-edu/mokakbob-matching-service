ace1b96dc8be1944780d5fdb58659338a6a9a530
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-03 22:37:44 +0900

    [FEAT] 매칭 확정 이벤트 발행 로직 구현

--- mokakbob-api/src/main/java/com/mokakbob/matching/exception/MatchingErrorCode.java
+++ mokakbob-api/src/main/java/com/mokakbob/matching/exception/MatchingErrorCode.java
@@ -6,7 +6,8 @@ public enum MatchingErrorCode implements ApiErrorCode {
     EXIST_MATCHING(400, "M001", "이미 매칭에 참여 중입니다."),
     MATCHING_OPERATION_INTERRUPTED(500, "M002", "매칭 작업이 시스템에 의해 중단되었습니다."),
     NOT_ENOUGH_MATCHING_POINT(400, "M003", "매칭에 참여할 포인트가 부족합니다."),
-    ALREADY_PROCESSING_NOTIFICATION(400, "M004", "이미 다른 요청에 의해 처리 중입니다.")
+    ALREADY_PROCESSING_NOTIFICATION(400, "M004", "이미 다른 요청에 의해 처리 중입니다."),
+    FAIL_PARSE_NOTIFICATION(500, "M005", "매칭 발견 이벤트를 파싱하는데 실패했습니다 ")
     ;
     private final int httpStatus;
     private final String customCode;
--- mokakbob-api/src/main/java/com/mokakbob/matching/service/MatchingNotificationService.java
+++ mokakbob-api/src/main/java/com/mokakbob/matching/service/MatchingNotificationService.java
@@ -1,14 +1,20 @@
 package com.mokakbob.matching.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mokakbob.cache.NotificationStore;
 import com.mokakbob.common.exception.exceptions.ApiException;
 import com.mokakbob.domain.matching.domain.Notification;
+import com.mokakbob.domain.matching.event.MatchingFoundEvent;
+import com.mokakbob.domain.matching.event.MatchingSuccessEvent;
 import com.mokakbob.matching.exception.MatchingErrorCode;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +26,8 @@ public class MatchingNotificationService {
 
     private final RedissonClient redissonClient;
     private final NotificationStore notificationStore;
+    private final ApplicationEventPublisher eventPublisher;
+    private final ObjectMapper objectMapper;
 
     public void lockNotificationRequest(Notification notification) {
         String idempotencyKey = notification.getKey();
@@ -43,25 +51,45 @@ public class MatchingNotificationService {
         }
     }
 
-    public void handleNotification(Notification notification) {
-        List<Notification> roomNotifications = notificationStore.findAllByKey(notification.getKey());
+    private void handleNotification(Notification notification) {
+        List<Notification> notifications = notificationStore.findAllByKey(notification.getKey());
 
-        if (roomNotifications.stream().anyMatch(n -> !n.isResponded())) {
+        if (notifications.stream().anyMatch(n -> !n.isResponded())) {
             return;
         }
 
-        if (roomNotifications.stream().allMatch(n -> Boolean.TRUE.equals(n.getAccepted()))) {
-            publishSuccessEvent(notification);
+        if (notifications.stream().allMatch(n -> Boolean.TRUE.equals(n.getAccepted()))) {
+            publishSuccessEvent(notifications);
         } else {
-           publishRejectEvent(notification);
+            publishRejectEvent(notification);
         }
     }
 
-    public void publishSuccessEvent(Notification notification) {
+    private void publishSuccessEvent(List<Notification> notifications) {
+        Notification notification = notifications.get(0);
+        MatchingFoundEvent event = parseEvent(notification);
 
+        MatchingSuccessEvent successEvent = new MatchingSuccessEvent(
+                event.key(),
+                event.category(),
+                event.participantCount(),
+                notifications.stream()
+                        .map(Notification::getMemberId).toList(),
+                Instant.now()
+        );
+
+        eventPublisher.publishEvent(successEvent);
     }
 
-    public void publishRejectEvent(Notification notification) {
+    private void publishRejectEvent(Notification notification) {
         // todo: matching.reject 토픽 발급 이벤트 publisher 구현
+    }
+
+    private MatchingFoundEvent parseEvent(Notification notification) {
+        try {
+            return objectMapper.readValue(notification.getPayload(), MatchingFoundEvent.class);
+        } catch (IOException e) {
+            throw new ApiException(MatchingErrorCode.FAIL_PARSE_NOTIFICATION);
+        }
     }
 }
