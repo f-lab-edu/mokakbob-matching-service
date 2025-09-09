1ecf43ee5a92bdb8de909642185e132409f49c97
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-09 13:53:25 +0900

    [FEAT] ec2 배포를 위한 설정 변경

--- mokakbob-redis/src/main/java/com/mokakbob/config/RedisConfig.java
+++ mokakbob-redis/src/main/java/com/mokakbob/config/RedisConfig.java
@@ -1,27 +1,28 @@
 package com.mokakbob.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
 
-    private final RedisSentinelProps props;
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port) {
+        return new LettuceConnectionFactory(host, port);
+    }
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-                .master(props.getMaster());
-
-        for (String node : props.getNodes()) {
-            String[] parts = node.split(":");
-            sentinelConfig.sentinel(parts[0], Integer.parseInt(parts[1]));
-        }
-
-        return new LettuceConnectionFactory(sentinelConfig);
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
--- mokakbob-redis/src/main/java/com/mokakbob/config/RedissonConfig.java
+++ mokakbob-redis/src/main/java/com/mokakbob/config/RedissonConfig.java
@@ -1,21 +1,31 @@
 package com.mokakbob.config;
 
 
-import java.io.IOException;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 @Configuration
 public class RedissonConfig {
 
     @Bean(destroyMethod = "shutdown")
-    public RedissonClient redissonClient(@Value("classpath:redisson.yml") Resource configFile) throws IOException {
-        Config config = Config.fromYAML(configFile.getInputStream());
+    public RedissonClient redissonClient(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port) {
+
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setConnectionMinimumIdleSize(10)
+                .setConnectionPoolSize(64)
+                .setIdleConnectionTimeout(10000)
+                .setTimeout(3000)
+                .setRetryAttempts(3)
+                .setRetryInterval(1500);
+
         return Redisson.create(config);
     }
 }
