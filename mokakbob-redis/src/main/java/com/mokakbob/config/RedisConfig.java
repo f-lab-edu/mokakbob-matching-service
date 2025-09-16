00277e3ee41fcb91af811b2a614d7bb6d8e58471
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-16 13:38:12 +0900

    [FEAT] redis docker 접속을 위한 설정 변경

--- mokakbob-redis/src/main/java/com/mokakbob/config/RedisConfig.java
+++ mokakbob-redis/src/main/java/com/mokakbob/config/RedisConfig.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -16,28 +13,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfig {
 
     @Bean
-    @Profile("local")
     public LettuceConnectionFactory redisConnectionFactory(
             @Value("${spring.data.redis.host}") String host,
             @Value("${spring.data.redis.port}") int port) {
         return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    @Profile("prod")
-    public LettuceConnectionFactory prodRedisConnectionFactory(
-            @Value("${spring.data.redis.host}") String host,
-            @Value("${spring.data.redis.port}") int port) {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(host);
-        config.setPort(port);
-
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .useSsl()
-                .disablePeerVerification()
-                .build();
-
-        return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
