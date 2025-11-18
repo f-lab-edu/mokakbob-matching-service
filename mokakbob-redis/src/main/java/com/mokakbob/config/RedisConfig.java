e7d9ccd6ccba3dca071e12c3cc9fbe4dcb727533
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-18 14:18:56 +0900

    [FEAT] redis config 설정 cluster 모드로 변경

--- mokakbob-redis/src/main/java/com/mokakbob/config/RedisConfig.java
+++ mokakbob-redis/src/main/java/com/mokakbob/config/RedisConfig.java
@@ -1,19 +1,43 @@
 package com.mokakbob.config;
 
-import lombok.RequiredArgsConstructor;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.cluster.RedisClusterClient;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
-@RequiredArgsConstructor
 public class RedisConfig {
 
+    private static final int DEFAULT_REDIS_PORT = 6379;
+    private static final int REDIS_TIMEOUT_SECONDS = 10;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory(
-            @Value("${spring.data.redis.host}") String host,
-            @Value("${spring.data.redis.port}") int port) {
-        return new LettuceConnectionFactory(host, port);
+    public RedisClusterClient redisClusterClient(
+            @Value("${spring.data.redis.cluster.nodes}") List<String> clusterNodes
+    ) {
+
+        List<RedisURI> uris = clusterNodes.stream()
+                .map(RedisConfig::toRedisURI)
+                .collect(Collectors.toList());
+
+        return RedisClusterClient.create(uris);
+    }
+
+    private static RedisURI toRedisURI(String node) {
+        String[] parts = node.split(":");
+        String host = parts[0];
+        int port = (parts.length > 1)
+                ? Integer.parseInt(parts[1])
+                : DEFAULT_REDIS_PORT;
+
+        return RedisURI.builder()
+                .withHost(host)
+                .withPort(port)
+                .withTimeout(Duration.ofSeconds(REDIS_TIMEOUT_SECONDS))
+                .build();
     }
 }
