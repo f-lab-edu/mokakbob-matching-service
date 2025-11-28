cc8b06ef563f13be06e6c0515e83ee07c08c5b5c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-28 22:56:31 +0900

    [FEAT] 페이징 조회를 위한 응답 구조 수정

--- mokakbob-api/src/main/java/com/mokakbob/chat/controller/response/ChatRoomResponses.java
+++ mokakbob-api/src/main/java/com/mokakbob/chat/controller/response/ChatRoomResponses.java
@@ -1,16 +1,29 @@
 package com.mokakbob.chat.controller.response;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 
 public record ChatRoomResponses(
         Long memberId,
-        List<ChatRoomResponse> rooms
+        List<ChatRoomResponse> rooms,
+        int page,
+        int size,
+        long totalElements,
+        int totalPages,
+        boolean last
 ) {
-
     public static ChatRoomResponses of(
             Long memberId,
-            List<ChatRoomResponse> rooms
+            Page<ChatRoomResponse> pageResult
     ) {
-        return new ChatRoomResponses(memberId, rooms);
+        return new ChatRoomResponses(
+                memberId,
+                pageResult.getContent(),
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.getTotalPages(),
+                pageResult.isLast()
+        );
     }
 }
