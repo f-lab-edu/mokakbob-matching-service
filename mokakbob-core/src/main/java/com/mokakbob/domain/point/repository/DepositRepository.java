db96f7f5041fdb60d637a960fef16a851e3a6a43
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-11 13:17:44 +0900

    [FEAT] 정책 변경에 따른 deposit 테이블 삭제

--- mokakbob-core/src/main/java/com/mokakbob/domain/point/repository/DepositRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mokakbob.domain.point.repository;
-
-import com.mokakbob.domain.point.domain.Deposit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepositRepository extends JpaRepository<Deposit, Long> {
-}
